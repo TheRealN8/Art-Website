d7362a8e68a806a439c0da92adc989e3f4dcebc7
TheRealN8 <113065002+TheRealN8@users.noreply.github.com>
2023-04-21 14:26:07 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Heading from "./components/Heading";
+
+
+const App = () =>{
+  return(
+    
+    <div>
+      <h1>Heading</h1> 
+    </div>
+    
+
+  )
+}
+
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Heading() {
+    
+    return (
+
+        <div className="heading">
+            <h1>Website Name</h1>
+        </div>
+        
+    )
+
+};
+
+export default Heading;
